06f70f1c2c58311e6e9f386a710804ad50d72153
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-24 13:06:23 +0100

    Check if item is sulfuras at beginning of function

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,11 +14,13 @@ class GildedRose {
 	}
 
 	private static void updateItem(Item item) {
+		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+			return;
+		}
+
 		if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 			if (item.quality > 0) {
-				if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-					item.quality = item.quality - 1;
-				}
+				item.quality = item.quality - 1;
 			}
 		} else {
 			if (item.quality < 50) {
@@ -40,17 +42,13 @@ class GildedRose {
 			}
 		}
 
-		if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-			item.sellIn = item.sellIn - 1;
-		}
+		item.sellIn = item.sellIn - 1;
 
 		if (item.sellIn < 0) {
 			if (!item.name.equals("Aged Brie")) {
 				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 					if (item.quality > 0) {
-						if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-							item.quality = item.quality - 1;
-						}
+						item.quality = item.quality - 1;
 					}
 				} else {
 					item.quality = 0;
