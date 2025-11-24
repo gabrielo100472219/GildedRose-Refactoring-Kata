3f3892d18638cf005bc5ad83327a4d05132b14d4
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-24 13:19:39 +0100

    Extract name checks for sulfuras and normal items

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -14,29 +14,26 @@ class GildedRose {
 	}
 
 	private static void updateItem(Item item) {
-		if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+		if (isSulfuras(item)) {
 			return;
 		}
 
-		if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-			if (item.quality > 0) {
-				item.quality = item.quality - 1;
-			}
-		} else {
-			if (item.quality < 50) {
-				item.quality = item.quality + 1;
+		if (isNormalItem(item) && item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+		if (!isNormalItem(item) && item.quality < 50) {
+			item.quality = item.quality + 1;
 
-				if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-					if (item.sellIn < 11) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
+			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+				if (item.sellIn < 11) {
+					if (item.quality < 50) {
+						item.quality = item.quality + 1;
 					}
+				}
 
-					if (item.sellIn < 6) {
-						if (item.quality < 50) {
-							item.quality = item.quality + 1;
-						}
+				if (item.sellIn < 6) {
+					if (item.quality < 50) {
+						item.quality = item.quality + 1;
 					}
 				}
 			}
@@ -47,17 +44,28 @@ class GildedRose {
 		if (item.sellIn < 0) {
 			if (!item.name.equals("Aged Brie")) {
 				if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+					// normal item
 					if (item.quality > 0) {
 						item.quality = item.quality - 1;
 					}
 				} else {
+					// backstage passes
 					item.quality = 0;
 				}
 			} else {
+				// aged brie
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
 				}
 			}
 		}
+	}
+
+	private static boolean isNormalItem(Item item) {
+		return !item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+	}
+
+	private static boolean isSulfuras(Item item) {
+		return item.name.equals("Sulfuras, Hand of Ragnaros");
 	}
 }
