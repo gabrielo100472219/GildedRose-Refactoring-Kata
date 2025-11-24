eaa68c04427ebf3b80d546544efde5ad87350dab
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-24 13:27:14 +0100

    Join nested ifs

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -25,16 +25,12 @@ class GildedRose {
 			item.quality = item.quality + 1;
 
 			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.sellIn < 11) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+				if (item.sellIn < 11 && item.quality < 50) {
+					item.quality = item.quality + 1;
 				}
 
-				if (item.sellIn < 6) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+				if (item.sellIn < 6 && item.quality < 50) {
+					item.quality = item.quality + 1;
 				}
 			}
 		}
@@ -42,19 +38,15 @@ class GildedRose {
 		item.sellIn = item.sellIn - 1;
 
 		if (item.sellIn < 0) {
-			if (item.name.equals("Aged Brie")) {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
+			if (item.name.equals("Aged Brie") && item.quality < 50) {
+				item.quality = item.quality + 1;
 			}
 			if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				item.quality = 0;
 			}
 
-			if (isNormalItem(item)) {
-				if (item.quality > 0) {
-					item.quality = item.quality - 1;
-				}
+			if (isNormalItem(item) && item.quality > 0) {
+				item.quality = item.quality - 1;
 			}
 		}
 	}
