346551ede1a33710e020bb990fe359532e688cef
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-26 13:12:07 +0100

    Refactor if statements in normal item update method

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -38,11 +38,7 @@ class GildedRose {
 			item.quality = item.quality - 1;
 		}
 
-		if (item.sellIn >= 0) {
-			return;
-		}
-
-		if (item.quality > 0) {
+		if (item.sellIn < 0 && item.quality > 0) {
 			item.quality = item.quality - 1;
 		}
 	}
