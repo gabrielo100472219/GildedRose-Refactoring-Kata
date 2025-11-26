b7f632f124234558c3d95e8cf89fcf1d56225a1e
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-26 13:01:41 +0100

    Exctract special items update logic

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -18,22 +18,18 @@ class GildedRose {
 			return;
 		}
 
-		if (isNormalItem(item) && item.quality > 0) {
-			item.quality = item.quality - 1;
+		if (isAgedBrie(item)) {
+			updateAgedBrie(item);
+			return;
 		}
 
-		if (!isNormalItem(item) && item.quality < 50) {
-			item.quality = item.quality + 1;
+		if (isBackstagePasses(item)) {
+			updateBackstagePasses(item);
+			return;
+		}
 
-			if (isBackstagePasses(item)) {
-				if (item.sellIn < 11 && item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-
-				if (item.sellIn < 6 && item.quality < 50) {
-					item.quality = item.quality + 1;
-				}
-			}
+		if (isNormalItem(item) && item.quality > 0) {
+			item.quality = item.quality - 1;
 		}
 
 		item.sellIn = item.sellIn - 1;
@@ -42,16 +38,48 @@ class GildedRose {
 			return;
 		}
 
-		if (isAgedBrie(item) && item.quality < 50) {
+		if (isNormalItem(item) && item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+	}
+
+	private static void updateAgedBrie(Item item) {
+		if (item.quality < 50) {
 			item.quality = item.quality + 1;
 		}
 
-		if (isBackstagePasses(item)) {
-			item.quality = 0;
+		item.sellIn--;
+
+		if (item.sellIn >= 0) {
+			return;
 		}
 
-		if (isNormalItem(item) && item.quality > 0) {
-			item.quality = item.quality - 1;
+		if (item.sellIn < 0 && item.quality < 50) {
+			item.quality++;
+		}
+	}
+
+	private static void updateBackstagePasses(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+
+			if (item.sellIn < 11 && item.quality < 50) {
+				item.quality = item.quality + 1;
+			}
+
+			if (item.sellIn < 6 && item.quality < 50) {
+				item.quality = item.quality + 1;
+			}
+		}
+
+		item.sellIn--;
+
+		if (item.sellIn >= 0) {
+			return;
+		}
+
+		if (item.sellIn < 0) {
+			item.quality = 0;
 		}
 	}
 
