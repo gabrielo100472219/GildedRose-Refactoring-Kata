984a7c5deafc0238bcac5a564d242d41334fce65
Gabriel Ortega <gabrielon@ext.inditex.com>
2025-11-26 13:10:29 +0100

    Extract normal item update logic to method

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -30,6 +30,10 @@ class GildedRose {
 			return;
 		}
 
+		updateNormalItem(item);
+	}
+
+	private static void updateNormalItem(Item item) {
 		if (item.quality > 0) {
 			item.quality = item.quality - 1;
 		}
